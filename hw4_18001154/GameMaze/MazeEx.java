5c42bf1fa4857f83506d774890d9ca58e6b4241d
Ha Vu Long <63302037+Orchids-Programe@users.noreply.github.com>
2020-06-12 22:52:01 +0700

    Add files via upload

---
+++ hw4_18001154/GameMaze/MazeEx.java
@@ -0,0 +1,30 @@
+package GameMaze;
+
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MazeEx {
+
+    public static void main(String[] agrs) {
+        Maze maze = new Maze();
+        maze.printConsole();
+        // System.out.println("Could find Way: " + maze.findWay());
+        // maze.backTracking(maze.endPoint);
+        
+        EventQueue.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    MazePanel mazePanel = new MazePanel();
+                    mazePanel.setVisible(true);
+                }catch (Exception e) {
+                    System.out.println("Somethings Wrong");
+                }
+            }
+        });
+    }
+}
